959e42bbb0bc9a583d73b3751f6a7304dfb3dcd5
lena-don <lena08don@gmail.com>
2023-03-23 01:34:17 +0300

    build

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,18 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
+ // console.log('Test')
+//
+//
+//тэги фильтрации на страницах: Библиотека, Статьи
+//
+//
+//открыть случайную статью — в разработке
 
+var pagesGroup = (/* unused pure expression or super */ null && ([]));
+
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 /******/ })()
 ;
